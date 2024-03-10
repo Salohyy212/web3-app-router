d8dbc40196389b920e12d68be10f9dbd5c1ea769
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-10 14:18:29 +0300

    fix: Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -5,7 +5,9 @@ import { useRouter } from 'next/router';
 const IndexPage: React.FC = () => {
   const router = useRouter();
 
-  
+  useEffect(() => {
+    router.push('/home');
+  }, []);
 
   return null;
 };
